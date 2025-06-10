299b87a9cce82f6d022920864c209beb7af23fe6
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-10 10:07:17 -0300

    feat: adicionado DadosCadastroMedico como record para facilitar a implementação do DTO

--- src/main/java/med/voll/api/medicos/DTO/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medicos/DTO/DadosCadastroMedico.java
@@ -1,6 +1,7 @@
-package med.voll.api.medicos.Model;
+package med.voll.api.medicos.DTO;
 
 import med.voll.api.endereco.Model.DadosEndereco;
+import med.voll.api.medicos.Model.Especialidade;
 
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
