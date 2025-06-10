859eda4617cf3f2811a134f9d169dab681e3775c
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-10 10:05:29 -0300

    feat : adicionado o model de DadosEndereco que se relaciona com DadosCadastroMedico

---
+++ src/main/java/med/voll/api/endereco/Model/DadosEndereco.java
@@ -0,0 +1,9 @@
+package med.voll.api.endereco.Model;
+
+public record DadosEndereco(String logradouro, String bairro, String cep,
+                            String cidade, String uf , String complemento,
+                            String numero) {
+
+
+
+}
